8b443e9413f6b3b618bb23c0b5471d98b18779e6
Ja Wise <ja@Jas-MacBook-Pro.local>
2017-05-30 16:04:44 +0200

    Add (setq recentf-auto-cleanup 'never).

--- init.el
+++ init.el
@@ -61,6 +61,7 @@
 (setq js2-strict-missing-semi-warning nil)
 
 (require 'recentf)
+(setq recentf-auto-cleanup 'never)
 (recentf-mode 1)
 (setq recentf-max-menu-items 50)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
