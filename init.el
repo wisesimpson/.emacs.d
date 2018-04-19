01a1d3f2d7b6568b43855a0cde7118a7d623519b
Ja <ja@Jas-MacBook-Pro.local>
2018-04-19 16:04:29 +0200

    Change theme to Monokai.

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-(load-theme 'ample t)
+(load-theme 'monokai t)
 
 ;; Set path environment only on macOS
 (when (memq window-system '(mac ns))
@@ -84,7 +84,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ample-theme exec-path-from-shell slime ample-theme magit company multi-web-mode js2-mode))))
+    (exec-path-from-shell slime monokai-theme magit company multi-web-mode js2-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
