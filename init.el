dc4f214692935f33681eb269e38e7ae275f74b2c
Ja <ja@Jas-MacBook-Pro.local>
2019-05-16 14:48:21 +0200

    Add scad-mode.

--- init.el
+++ init.el
@@ -84,4 +84,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (exec-path-from-shell slime monokai-theme magit company multi-web-mode js2-mode))))
+    (scad-mode exec-path-from-shell slime monokai-theme magit company multi-web-mode js2-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
