8dd2fc067cdfd1d2ea4fbe5db3dd0ce7ddcc1af0
Zijar Wong <wise.simpson@gmail.com>
2019-05-17 00:10:52 +0200

    General optimization.

--- init.el
+++ init.el
@@ -1,10 +1,3 @@
-(if (functionp 'menu-bar-mode)
-    (menu-bar-mode -1))
-(if (functionp 'tool-bar-mode)
-    (tool-bar-mode -1))
-(if (functionp 'scroll-bar-mode)
-    (scroll-bar-mode -1))
-
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/"))
@@ -82,6 +75,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(menu-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(scroll-bar-mode nil)
  '(package-selected-packages
    (quote
     (scad-mode exec-path-from-shell slime monokai-theme magit company multi-web-mode js2-mode))))
