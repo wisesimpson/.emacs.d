7e9749505a825620363c224be3ae90697f1b9796
Ja <ja@Jas-MacBook-Pro.local>
2019-06-06 10:41:16 +0200

    Clean up some setups. The purpose is to make Emacs more flexible and mainly run in Terminal.

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-(set-face-attribute 'default (selected-frame) :height 125)
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/"))
@@ -21,28 +20,48 @@
     (scad-mode exec-path-from-shell slime monokai-theme magit company multi-web-mode js2-mode))))
 (package-install-selected-packages)
 
-(load-theme 'monokai t)
+;; (load-theme 'monokai t)
 
 ;; Set path environment only on macOS
-(when (memq window-system '(mac ns))
-  (unless (package-installed-p 'exec-path-from-shell)
-    (package-install 'exec-path-from-shell))
-  (require 'exec-path-from-shell)
-  (exec-path-from-shell-initialize))
+;; (when (memq window-system '(mac ns))
+;;   (unless (package-installed-p 'exec-path-from-shell)
+;;     (package-install 'exec-path-from-shell))
+;;   (require 'exec-path-from-shell)
+;;   (exec-path-from-shell-initialize))
 
 ;; highlight current line
-(global-hl-line-mode)
+;; (global-hl-line-mode +1)
 
 ;; line number
-(global-linum-mode t)
-(setq linum-format
-      #'(lambda (line)
-          (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
-            (propertize (format (format " %%%dd " w) line) 'face 'linum))))
+(global-display-line-numbers-mode)
+;; (global-linum-mode t)
+;; (setq linum-format
+;;       #'(lambda (line)
+;;           (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
+;;             (propertize (format (format " %%%dd " w) line) 'face 'linum))))
 
 ;; visual line mode
 (global-visual-line-mode 1)
-(setq-default line-spacing 0.4)
+;; (setq-default line-spacing 0.4)
+;; Set the padding between lines
+;; (defvar line-padding 3)
+;; (defun add-line-padding ()
+;;   "Add extra padding between lines"
+;;   ;; remove padding overlays if they already exist
+;;   (let ((overlays (overlays-at (point-min))))
+;;     (while overlays
+;;       (let ((overlay (car overlays)))
+;;         (if (overlay-get overlay 'is-padding-overlay)
+;;             (delete-overlay overlay)))
+;;       (setq overlays (cdr overlays))))
+;;   ;; add a new padding overlay
+;;   (let ((padding-overlay (make-overlay (point-min) (point-max))))
+;;     (overlay-put padding-overlay 'is-padding-overlay t)
+;;     (overlay-put padding-overlay 'line-spacing (* .1 line-padding))
+;;     (overlay-put padding-overlay 'line-height (+ 1 (* .1 line-padding))))
+;;   (setq mark-active nil))
+
+;; (add-hook 'buffer-list-update-hook 'add-line-padding)
 
 (add-hook 'after-init-hook 'global-company-mode)
 
@@ -54,8 +73,8 @@
 (setq recentf-max-saved-items 1000)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
-(setq less-css-compile-at-save t)
-(setq less-css-lessc-options (quote ("--no-color -x")))
+;; (setq less-css-compile-at-save t)
+;; (setq less-css-lessc-options (quote ("--no-color -x")))
 
 (setq inferior-lisp-program "sbcl")
 
@@ -63,18 +82,14 @@
 (slime-setup '(slime-repl))
 (define-key slime-mode-map (kbd "C-c e") 'slime-eval-buffer)
 
-(require 'multi-web-mode)
-(setq mweb-default-major-mode 'html-mode)
-(setq mweb-tags '((js-mode "<script[^>]*>" "</script>")
-                  (css-mode "<style>" "</style>")))
-(setq mweb-filename-extensions '("html"))
-(multi-web-global-mode 1)
+;; (require 'multi-web-mode)
+;; (setq mweb-default-major-mode 'html-mode)
+;; (setq mweb-tags '((js-mode "<script[^>]*>" "</script>")
+;;                   (css-mode "<style>" "</style>")))
+;; (setq mweb-filename-extensions '("html"))
+;; (multi-web-global-mode 1)
 
 ;; key binding
-(when (eq system-type 'darwin)
-  (global-set-key (kbd "C-s-f") 'toggle-frame-fullscreen)
-  (global-set-key (kbd "<C-s-268632070>") 'toggle-frame-fullscreen))
-
-(require 'tramp)
-(set-default 'tramp-auto-save-directory "C:\\Users\\ja\\AppData\\Local\\Temp")
-(set-default 'tramp-default-method "plink")
+;; (when (eq system-type 'darwin)
+;;   (global-set-key (kbd "C-s-f") 'toggle-frame-fullscreen)
+;;   (global-set-key (kbd "<C-s-268632070>") 'toggle-frame-fullscreen))
