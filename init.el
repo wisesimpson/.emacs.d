970d3190c9ada0d4b53affce52cf28f571c3176e
Wise <wise.simpson@gmail.com>
2020-03-14 16:16:45 +0100

    Change to tango-dark theme.

--- init.el
+++ init.el
@@ -12,11 +12,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark)))
  '(menu-bar-mode nil)
- '(scroll-bar-mode nil)
- '(package-selected-packages
-   (quote
-    (slime magit company))))
+ '(package-selected-packages (quote (slime magit company)))
+ '(scroll-bar-mode nil))
 (package-install-selected-packages)
 
 ;; (load-theme 'monokai t)
@@ -77,3 +76,9 @@
 (require 'slime)
 (slime-setup '(slime-repl))
 (define-key slime-mode-map (kbd "C-c e") 'slime-eval-buffer)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
