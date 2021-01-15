a73d6504cd7674bc9949fbd7d073f6f0db480625
Ja <ja@live.com>
2021-01-15 19:10:47 +0100

    Try theme "tsdh-dark".

--- init.el
+++ init.el
@@ -12,7 +12,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wheatgrass)))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes (quote (tsdh-dark)))
  '(menu-bar-mode nil)
  '(package-selected-packages (quote (slime magit company)))
  '(scroll-bar-mode nil))
