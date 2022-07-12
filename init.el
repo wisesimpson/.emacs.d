a9c984ec4646ca5cc2dcd9e7d25be8ab21dc34ec
Ja <ja@live.com>
2022-07-12 04:09:05 +0200

    Add paste-to-osx

--- init.el
+++ init.el
@@ -3,6 +3,13 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+(setq interprogram-cut-function 'paste-to-osx)
+
 ;; Fetch the list of packages available 
 (unless package-archive-contents
   (package-refresh-contents))
